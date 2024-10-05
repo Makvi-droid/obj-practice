4c84595b7d375b3d4416711d799948c25b36ee15
Makvi-droid <pasbenauro05@gmail.com>
2024-10-05 09:26:51 +0800

    added new files

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const person = {
-    name: 'Ben Dover',
+    name: {
+        first: 'Ben',
+        last: 'Dover'
+    },
     age: 24,
     height: '1.78',
     contactNumber: 93283,
     intro: function(){
        console.log(`Hello I am ${this.name} and I am ${this.age} years old`);
+    },
+
+    address(city){
+        console.log(`I live in ${person[city]}`);
     }
     
 };
-person.intro();
+
