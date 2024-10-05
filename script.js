2729e07254d51af5f0b7f529fadf3bdcfaf92a08
Makvi-droid <pasbenauro05@gmail.com>
2024-10-05 08:51:37 +0800

    test

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const person = {};
+const person = {
+    name: 'Ben Dover',
+    age: 24,
+    height: '1.78',
+    contactNumber: 93283,
+    intro: function(){
+       console.log(`Hello I am ${this.name} and I am ${this.age} years old`);
+    }
+    
+};
+person.intro();
