548d10145f4f2259d138106916e8d21a3bf1fb47
Makvi-droid <pasbenauro05@gmail.com>
2025-01-28 11:47:16 +0800

    added new

--- book.js
+++ book.js
@@ -3,4 +3,12 @@ function book(title, author, pages, readStatus){
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+
+    this.info = function(){
+        console.log(`${title} by ${author}, ${pages} pages, ${readStatus}`);
+    }
 }
+
+const person1 = new book('Bloodborne', 'Hidetaka Miyazaki', 278, 'read');
+
+person1.info();
